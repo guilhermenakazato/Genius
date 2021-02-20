558dc6d505f99758de77f07da57abfbc54f033f3
Guilherme Nakazato <guilherme.fernandes23903@gmail.com>
2021-02-20 00:41:27 -0300

    Adicionando navegação ao tocar no Card de projetos

--- frontend/lib/screens/main/project/project_info.dart
+++ frontend/lib/screens/main/project/project_info.dart
@@ -2,10 +2,28 @@ import 'package:flutter/material.dart';
 
 // TODO: documentar
 class ProjectInfo extends StatelessWidget {
+  final int number;
+
+  const ProjectInfo({Key key, this.number}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      backgroundColor: Theme.of(context).cardColor,
+      body: Align(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Projeto $number",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
