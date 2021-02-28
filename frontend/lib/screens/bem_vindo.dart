da07d6972635a0b99bc52172a6f4a1baef6ee4b3
Guilherme Nakazato <guilherme.fernandes23903@gmail.com>
2021-02-27 23:46:22 -0300

    Iniciando implementação de wrap no Genius

--- frontend/lib/screens/bem_vindo.dart
+++ frontend/lib/screens/bem_vindo.dart
@@ -12,11 +12,23 @@ class BemVindo extends StatelessWidget {
       },
       child: Scaffold(
         backgroundColor: const Color(0xff000000),
-        body: Stack(
-          children: <Widget>[
-            Transform.translate(
-              offset: Offset(15.0, 280.0),
-              child: Container(
+        body: Align(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+               Text(
+                'Bem\nVindo!',
+                style: TextStyle(
+                  fontFamily: 'Gotham',
+                  fontSize: 70,
+                  letterSpacing: 1.5,
+                  color: Theme.of(context).primaryColor,
+                  fontWeight: FontWeight.w900,
+                  height: 1.0,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              Container(
                 width: 310.0,
                 height: 320.0,
                 decoration: BoxDecoration(
@@ -26,26 +38,8 @@ class BemVindo extends StatelessWidget {
                   ),
                 ),
               ),
-            ),
-            Transform.translate(
-              offset: Offset(0, 130.0),
-              child: SizedBox(
-                width: 410.0,
-                child: Text(
-                  'Bem\nVindo!',
-                  style: TextStyle(
-                    fontFamily: 'Gotham',
-                    fontSize: 70,
-                    letterSpacing: 1.5,
-                    color: Theme.of(context).primaryColor,
-                    fontWeight: FontWeight.w900,
-                    height: 1.0,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
