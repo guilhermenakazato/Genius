970be90871fd68ea1e3417048008c6512d8a9765
Guilherme Nakazato <guilherme.fernandes23903@gmail.com>
2021-03-30 23:10:58 -0300

    [NEW] Conectando backend com BD Postgres Heroku

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -19,7 +19,12 @@
 */
 
 import Route from '@ioc:Adonis/Core/Route'
-
-Route.get('/', async () => {
-  return { hello: 'world' }
+import HealthCheck from '@ioc:Adonis/Core/HealthCheck'
+ 
+Route.get('health', async ({ response }) => {
+  const report = await HealthCheck.getReport()
+  
+  return report.healthy
+    ? response.ok(report)
+    : response.badRequest(report)
 })
