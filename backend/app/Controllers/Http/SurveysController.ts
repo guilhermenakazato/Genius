69fee64bd9d6918818141696ba97d3e364ca678c
Guilherme Nakazato <guilherme.fernandes23903@gmail.com>
2021-04-25 17:29:05 -0300

    [NEW] Configurando as rotas

--- backend/app/Controllers/Http/SurveysController.ts
+++ backend/app/Controllers/Http/SurveysController.ts
@@ -1,18 +1,27 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Survey from 'App/Models/Survey';
-import SurveyView from 'App/Views/SurveyView';
+import User from 'App/Models/User';
 
 export default {
-  async create({request}: HttpContextContract){
-    const surveyData = request.all();
-    const survey = await Survey.create(surveyData)
+  async create({request, params}: HttpContextContract){
+    const survey = request.all();
+    const {userId} = params;
 
-    return SurveyView.render(survey);
+    const user = await User.findOrFail(userId)
+    await user.related("surveys").create(survey)
+    await user.preload("surveys")
+
+    return user
   },
   async listAllSurveys(){
     return await Survey.all()
   },
   async getSurveyById({params}: HttpContextContract){
+    const {id} = params
+    const survey = await Survey.findOrFail(id)
 
+    await survey.preload("user")
+
+    return survey
   }
 }
