6ee04f8d84820cac544ae86842f87618fcadbd16
Guilherme Nakazato <guilherme.fernandes23903@gmail.com>
2021-06-11 03:25:10 -0400

    [NEW] Changing tags to implement on app

--- backend/database/migrations/1619379785147_tags.ts
+++ backend/database/migrations/1619379785147_tags.ts
@@ -6,7 +6,7 @@ export default class Tags extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.string("name", 400).notNullable()
+      table.string("name", 400).notNullable().unique()
       table.timestamps(true)
     })
   }
