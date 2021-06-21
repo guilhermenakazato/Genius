d367779262ea99227cb9c9fbfd8321c17db632b7
Guilherme Nakazato <guilherme.fernandes23903@gmail.com>
2021-06-20 22:24:01 -0400

    [NEW] Trying to configure email server in Genius

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -37,6 +37,7 @@ Route.post("/user",'UsersController.createUser');
 Route.delete("/user/:id", 'UsersController.deleteUser');
 Route.put("/user/:id", 'UsersController.updateUser'); 
 Route.post("/save-project", 'UsersController.saveProject')
+Route.post("/send-email", 'UsersController.sendEmail')
 
 Route.post("/login", 'AuthController.login');
 Route.get("/token", 'AuthController.authenticateWithToken');
